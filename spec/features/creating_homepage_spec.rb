6273451e0d338d1825e5207243dd92ab4bd03233
Paweł <tatarynowiczpawel2@gmail.com>
2019-06-22 17:47:34 +0200

    Implement home page

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature "Creating home page" do
   scenario do
     visit "/"
 
-    click_link "Home"
-    click_link "Athletes Den"
+    expect(page).to have_link "Home"
+    expect(page).to have_link "Athletes Den"
     expect(page).to have_content("Workout Lounge!")
     expect(page).to have_content("Show off your workout")
   end
