20004cb96c23cba189900a15e4b748b57ee6fc73
Paweł <tatarynowiczpawel2@gmail.com>
2019-06-26 22:11:20 +0200

    Create chatroom when user signs up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_182203) do
+ActiveRecord::Schema.define(version: 2019_06_26_200628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2019_06_26_182203) do
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -49,4 +57,5 @@ ActiveRecord::Schema.define(version: 2019_06_26_182203) do
 
   add_foreign_key "exercises", "users"
   add_foreign_key "friendships", "users"
+  add_foreign_key "rooms", "users"
 end
